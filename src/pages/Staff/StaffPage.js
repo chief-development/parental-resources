1c00c726b3cd2bb5fd5dc0d7a89c7df5ceacde0c
curro casado <currocuo9625@gmail.com>
2019-03-23 12:22:14 -0500

    Auto stash before merge of "cc/8-staff-page" and "origin/master"

--- src/pages/Staff/StaffPage.js
+++ src/pages/Staff/StaffPage.js
@@ -1,5 +1,39 @@
 import React from "react";
 
+const sports = [""];
+
 export default function StaffPage() {
-  return <div className="Header">Staff!</div>;
+  return (
+    <>
+      <h1> Men's Sports</h1>
+      <ul>
+        <li>
+          <a
+            href="https://lindenwoodlions.com/sports/2014/7/21/MSWIM_0721142822.aspx"
+            target="__blank"
+          >
+            Swimming and Diving
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://lindenwoodlions.com/sports/2014/7/11/BB_0711141948.aspx"
+            target="__blank"
+          >
+            Baseball
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://lindenwoodlions.com/sports/2014/6/19/MBB_0619143018.aspx"
+            target="__blank"
+          >
+            Baseketball
+          </a>
+        </li>
+      </ul>
+
+      <h1> Women's Sports</h1>
+    </>
+  );
 }
