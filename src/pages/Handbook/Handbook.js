10eea7447d7dfc857f2094c7b35a4e508a3f89a7
Harrison <h.york11@gmail.com>
2019-03-23 11:41:05 -0500

    Adding links

--- src/pages/Handbook/Handbook.js
+++ src/pages/Handbook/Handbook.js
@@ -3,8 +3,19 @@ import React from "react";
 export default function Handbook() {
   return (
     <div className="Header">
-      <h3>Handbook</h3>
-      This is where we will link the handbooks to
+      <a
+        href="https://lindenwoodlions.com/documents/2018/9/25/NCAA_2018_19_Student_Athlete_Handbook.pdf"
+        target="_blank"
+      >
+        <h6>NCAA Handbook</h6>
+      </a>
+      <br />
+      <a
+        href="https://lindenwoodlions.com/documents/2018/9/24/SLS_Student_Athlete_Handbook_SLS_.pdf"
+        target="_blank"
+      >
+        <h6>Student Life Sports Handbook</h6>
+      </a>
     </div>
   );
 }
