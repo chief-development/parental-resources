a618d94bf1d4d5104a63394969e3142e327ac060
Daniel Chavez <dcmejia60@gmail.com>
2019-04-05 18:07:09 -0500

    lu logo path changed

--- src/pages/Admin/LogIn.js
+++ src/pages/Admin/LogIn.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./login.css";
-import LuLogo from "/Users/danielchavez/Documents/GitHub/parental-resources/src/img/LU_Logo.png";
+import LuLogo from "./LU_Logo.png";
 import { Link } from "react-router-dom";
 
 export default function LogIn() {
