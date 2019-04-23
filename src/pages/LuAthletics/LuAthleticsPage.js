11fc006dbc605fd8dd33ec5ace0877a1d7650d3c
Harrison <h.york11@gmail.com>
2019-04-23 10:05:05 -0500

    Adding pages

--- src/pages/LuAthletics/LuAthleticsPage.js
+++ src/pages/LuAthletics/LuAthleticsPage.js
@@ -1,16 +1,45 @@
 import React from "react";
 import Footer from "./footer";
-export default function LuAthletics() {
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    backgroundColor: "light-gray"
+  }
+});
+
+function PaperSheet(props) {
+  const { classes } = props;
   return (
-    <div className="Header">
-      <a
-        href="http://www.lindenwoodlions.com"
-        target="__blank"
-        rel="noopener noreferrer"
-      >
-        {"Lindenwood Athletics"}
-      </a>
-      <Footer />
+    <div>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="h5" component="h3">
+          <a
+            href="http://www.lindenwoodlions.com"
+            target="__blank"
+            rel="noopener noreferrer"
+          >
+            {"Lindenwood Athletics"}
+          </a>
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your
+          application.
+        </Typography>
+
+        <Footer />
+      </Paper>
     </div>
   );
 }
+PaperSheet.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(PaperSheet);
